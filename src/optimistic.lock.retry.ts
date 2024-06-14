adcd1aae799e4b9e844844ad915e9e9716fa45d9
Je-Hwan Yoo <yoojehwan1875@gmail.com>
2024-06-14 15:19:35 +0900

    feat: implements Retry type

---
+++ src/optimistic.lock.retry.ts
@@ -0,0 +1,43 @@
+import { Retry } from './optimistic.lock.retry.type.js'
+
+/**
+ *
+ * @param maxAttempt 최대 시도 회수
+ * @param delay 재시도 시 딜레이
+ */
+export const retry: Retry =
+  (maxAttempt, delay) =>
+  /**
+   *
+   * @param fn 실행할 함수
+   */
+  fn =>
+  /**
+   *
+   * @param backoff 실패시 실행할 함수
+   */
+  backoff =>
+  (...args) => {
+    const attemptFn = (attempt: number) => {
+      try {
+        const result = fn(...args)
+        if (result instanceof Promise) {
+          return result.catch(e => handleError(e, attempt))
+        }
+        return result
+      } catch (e) {
+        return handleError(e, attempt)
+      }
+    }
+
+    const handleError = (e: unknown, attempt: number) => {
+      if (attempt === maxAttempt) {
+        return backoff(e)
+      }
+      return new Promise(resolve =>
+        setTimeout(() => resolve(attemptFn(attempt + 1)), delay),
+      )
+    }
+
+    return attemptFn(0)
+  }
