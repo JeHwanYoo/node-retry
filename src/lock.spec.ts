a11a584eaca68c658a41ee6bb87185bb25139a37
Je-Hwan Yoo <yoojehwan1875@gmail.com>
2024-06-14 15:22:19 +0900

    test: should success with non-promise type

--- src/lock.spec.ts
+++ src/lock.spec.ts
@@ -1,5 +1,13 @@
-import { expect, it } from 'vitest'
+import { describe, expect, it } from 'vitest'
+import { retry } from './optimistic.lock.retry.js'
 
 it('ok', () => {
   expect(true).toBeTruthy()
+})
+
+describe('retry function', () => {
+  it('should succeed with a non-promise type', () => {
+    const return10 = retry(10, 100)(() => 10)(console.error)
+    expect(return10()).toBe(10)
+  })
 })
